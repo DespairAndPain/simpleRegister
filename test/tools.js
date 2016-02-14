2cd71c5cb0ba2699af22832e3bb3214b90e05f53
Stepan <batbutbait@gmail.com>
2016-02-14 14:39:38 +0500

    + тесты

--- test/tools.js
+++
@@ -1,9 +0,0 @@
-describe('Truth', function () {
-  it('should be true', function () {
-    true.should.be.true
-  })
-
-  it('should not be false', function () {
-    true.should.not.be.false
-  })
-})
